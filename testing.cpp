de547959ea69a30a4cfe2879964e9f77a6169104
kalaiyarasan-2004 <kalaiyarasan.offl.2004@gmail.com>
2024-07-13 21:45:54 +0530

    kalai

---
+++ testing.cpp
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+int main(){
+	char str_inp[20],result[10];
+	int length;
+	printf("Enter your password : ");
+	scanf("%s",str_inp);
+	//result = "4 days";
+	strcpy(result,"dff");
+	printf("%s",result);
+}
+
